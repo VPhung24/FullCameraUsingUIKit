334ad540ba1f60da3ffb608ec64357084cd3e280
Vivian Phung <9276753+VPhung24@users.noreply.github.com>
2023-03-20 21:31:16 -0400

    Initial Commit

---
+++ WhatChaUp2UsingUIKit/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  WhatChaUp2UsingUIKit
+//
+//  Created by Vivian Phung on 3/20/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
